d60b9030f3a664fef5739fdc2c96407a4b658f5b
Kathrina-dev <kathrinaelangbam@gmail.com>
2025-10-17 21:38:21 +0530

    updated prisma .user.create

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ async function main() {
     const newUser = await prisma.user.create({
         data: {
             username: "JohnDoe",
+            password: "securepassword123",
             tasks: {
                 create: [
                     {task: "Complete this stupid project"}
