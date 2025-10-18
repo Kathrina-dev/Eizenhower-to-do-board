258cf0f46d94068676761250e41a535a1ab568b1
Kathrina-dev <kathrinaelangbam@gmail.com>
2025-10-18 08:20:58 +0530

    updated routes in frontend

--- Eizenhower/src/LoginPage.jsx
+++ Eizenhower/src/LoginPage.jsx
@@ -70,7 +70,7 @@ const LoginPage = ({ onLogin }) => {
           <p className="text-sm text-gray-600">
             Don’t have an account?{" "}
             <a
-              href="http://localhost:5174/signup"
+              href="https://eizenhower-to-do-board-backend.onrender.com/signup"
               className="text-blue-500 hover:underline"
             >
               Register here
