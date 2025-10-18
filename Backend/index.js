3930398de19d62485aedeece6ca0fab8428c93ba
Kathrina-dev <kathrinaelangbam@gmail.com>
2025-10-18 08:01:02 +0530

    updated middleware

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import  userRoutes  from "./routes/user.js";
 import  taskRoutes  from "./routes/task.js";
 import { createUserOnStart } from "./server.js";
+import { isLoggedIn } from "./middlewares/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -27,6 +28,10 @@ app.use("/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
   res.send("✅ Server is running!");
+});
+
+app.get("/protected", isLoggedIn(), (req, res) => {
+    res.send("✅ You are logged in!");
 });
 
 // Initialize database, then start server
--- Backend/middleswares/index.js
+++ Backend/middleswares/index.js
@@ -1,14 +1,12 @@
-function isLoggedIn(){
-    return(req, res, next) => {
+// middlewares/index.js
+export function isLoggedIn() {
+    return (req, res, next) => {
         const userID = parseInt(req.headers["id"]); 
 
-        if(isNaN(userID)) return res.status(401).json({ msg: "Unauthorized: User not logged in or invalid ID" });
-        else{
+        if (isNaN(userID)) {
+            return res.status(401).json({ msg: "Unauthorized: User not logged in or invalid ID" });
+        } else {
             next();
         }
     };
-};
-
-module.exports = {
-    isLoggedIn,
 }
